856b2b92a2cfde181d31ffe517f27b4d6068d031
daniel-navarro-ls <d.navarrom.2023@alumnos.urjc.es>
2025-09-30 16:11:35 +0200

    Añadido Application.java
    con este archivo se lanza la aplicaion en localhost:8080

---
+++ src/main/java/com/example/retosflags/RetosFlagApplication.java
@@ -0,0 +1,12 @@
+package com.example.retosflags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetosFlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetosFlagApplication.class, args);
+	}
+}
